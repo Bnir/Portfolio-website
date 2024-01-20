44bb57ab3d98fafdb5c628f40d2adc7eb8a34ef5
HImanshu <himanshupoojary0@gmail.com>
2024-01-20 23:19:33 +0530

    Added ejs module

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+
+const app = express();
+const port= 5050;
+
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+
+
+
+
+app.listen(port,(err)=>{
+    if (err) {
+        console.log('Error laude')
+    }else{
+        console.log('Listening at port 5050')
+    }
+    
+})
